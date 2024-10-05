f098e58079ee237074b6bf574de3e431f194aad4
gabriel-victor933 <gabrielvictor933@usp.br>
2024-10-05 18:35:53 -0300

    feat: todas as tabelas e quantidade de filmes disponiveis

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pandas 
 
 movie_data = pandas.read_csv("netflix_titles.csv")
+
+# Obtendo todas as colunas
+columns = movie_data.columns.values
+print(f"A tabela possui as seguintes colunas: {columns}")
+
+# Quantidade de filmes disponiveis
+movies = movie_data[movie_data.type == "Movie"]
+print(f"A Quantidade de filmes Disponiveis é: {movies}")
